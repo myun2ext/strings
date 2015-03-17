43b85bd591275af5d12541f7abe619062593eedb
myun2 <myun2@nwhite.info>
2015-03-17 20:46:06 +0900

    with sizing

--- test.cpp
+++ test.cpp
@@ -1,10 +1,20 @@
 #include "strings.hpp"
+#include "with_size.hpp"
 
 using namespace myun2::memorize;
 
-int main() {
+int main()
+{
 	pool_file<42> f("example.db");
-	printf("wrote: %d\n", f.write("aaabbb\n"));
-	printf("read: %s", f.read(0, 7).c_str());
+
+	int pos;
+	printf("wrote: %d\n", pos = f.write("aaabbb\n"));
+	printf("read: %s", f.read(pos, 7).c_str());
+
+	//	With size
+	short_string s("abcdefgabcdefgabcdefg0000\n");
+	printf("wrote: %d\n", pos = s.write(f));
+	printf("read: %s", s.read(f, pos).c_str());
+
 	return 0;
 }
---
+++ with_size.hpp
@@ -0,0 +1,54 @@
+#ifndef __MYUN2__MEMORIZE__SIZED__HPP__
+#define __MYUN2__MEMORIZE__SIZED__HPP__
+
+#include <string>
+#include <stdio.h>
+
+namespace myun2
+{
+	namespace memorize
+	{
+		template <typename T, unsigned char _SizeBytes=1>
+		struct with_size
+		{
+			size_t size;
+			T* buffer;
+
+			template <typename Pool>
+			long write(Pool &pool)
+			{
+				long pos = pool.write(&size, _SizeBytes);
+				pool.write(buffer, size);
+				return pos;
+			}
+			template <typename Pool>
+			void read(Pool &pool, long pos)
+			{
+				pool.read(pos, &size, _SizeBytes);
+				pool.read(pos + _SizeBytes, buffer, size);
+			}
+		};
+
+		struct short_string : public with_size<char, 1>
+		{
+			::std::string str;
+			short_string(const char* s) : str(s) {}
+
+			template <typename Pool>
+			long write(Pool &pool)
+			{
+				size = str.size();
+				buffer = (char*)str.data();
+				return with_size<char, 1>::write(pool);
+			}
+			template <typename Pool>
+			::std::string read(Pool &pool, long pos)
+			{
+				with_size<char, 1>::read(pool, pos);
+				return str = ::std::string(buffer, size);
+			}
+		};
+	}
+}
+
+#endif//__MYUN2__MEMORIZE__SIZED__HPP__
