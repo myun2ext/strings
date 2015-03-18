0f578babeb8b4c7d20f9b331a0703efe04adc631
myun2 <myun2@nwhite.info>
2015-03-18 19:46:25 +0900

    users

--- users_table.cpp
+++ users_table.cpp
@@ -1,40 +1,31 @@
 #include "table.hpp"
+#include "with_size.hpp"
 #include <string>
 
 using namespace myun2::memorize;
 
 class users_table : public file_pool_table<4>
 {
+private:
+	typedef file_pool_table<4> _Base;
 public:
 	users_table() : file_pool_table<4>("users") {}
 
-	struct id_type {
-		unsigned int id;
-	};
-	struct name_type {
-		::std::string name;
-	};
-	struct screen_name_type {
-		::std::string screen_name;
-	};
-	struct flags_type {
-		unsigned int flags;
-		flags_type() { flags = 0; }
-	};
 	struct user
 	{
-		id_type id;
-		name_type name;
-		screen_name_type screen_name;
-		flags_type flags;
+		unsigned int id;
+		short_string name;
+		short_string screen_name;
+		unsigned int flags;
 		user(const char* _name, const char* _screen_name) {
-			id.id = 0;
-			name.name = _name;
-			screen_name.screen_name = _screen_name;
+			id = 0;
+			name = _name;
+			screen_name = _screen_name;
 		}
 	};
 
 	void add(const user& u) {
+		insert(u.id, u.name, u.screen_name, u.flags);
 	}
 };
 typedef users_table::user user;
